9ff45125c7382b3b356287e86328d61dd43ab619
nate <nathaniel.may22@gmail.com>
2020-08-16 14:14:23 -0400

    updated env file for deployments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC1nrIiriDfMaeMdEAdfgXAcmxkYGSay90',
+    authDomain: 'tasks-e8e9b.firebaseapp.com',
+    databaseURL: 'https://tasks-e8e9b.firebaseio.com',
+    projectId: 'tasks-e8e9b',
+    storageBucket: 'tasks-e8e9b.appspot.com',
+    messagingSenderId: '323102633904',
+    appId: '1:323102633904:web:2137aecfe1ac3aa7867075',
+    measurementId: 'G-97VHR8ST10'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
     messagingSenderId: '323102633904',
     appId: '1:323102633904:web:2137aecfe1ac3aa7867075',
     measurementId: 'G-97VHR8ST10'
-}
+  }
 };
 
 /*
